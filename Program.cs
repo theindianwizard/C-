9269efb34193a23c2e055a9a55c45b12b94adeb5
THEINDIANWIZARD <guttymanswaggg@hotmail.com>
2021-12-09 20:39:06 +0000

    commit 14: partly completed character class

--- Program.cs
+++ Program.cs
@@ -1,7 +1,33 @@
 ﻿namespace Character
 {
+    // ? Make other characters (neutral) ?
+    // TODO: Make protagonist and antagonist complete with character type (shy, funny, intelligent) and add removoe methods
     class protagonist
     {
-        public static string[] protagonists = new string[] {};
+        List<String> protagonists = new List<string>() {};
+        public protagonist(int hp, string type, int luck)
+        {
+            
+        }
+        public void remove(string listitem)
+        {
+            protagonists.Remove(listitem);
+        }
+    }
+    class antagonist
+    {
+        List<String> antagonists = new List<string>() {};
+        public antagonist(int hp, string type, int badluck)
+        {
+            
+        }
+        public void remove(string listitem)
+        {
+            antagonists.Remove(listitem);
+        }
+    }
+    class NPC
+    {
+        
     }
 }
